356e17fe2e9013f661ad327860e30f9b78f00919
Nayabkhan123 <160863567+Nayabkhan123@users.noreply.github.com>
2024-08-05 21:51:22 +0530

    Add files via upload

--- 2053_KthDistinctStringInAnArray.cpp
+++ 2053_KthDistinctStringInAnArray.cpp
@@ -1,3 +1,22 @@
+//optimised approch
+string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string,int>mp;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(mp[arr[i]]==1){
+                cnt++;
+                if(cnt==k) return arr[i];
+            }
+        }
+        return "";
+    }
+
+
+// brute force approch
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
